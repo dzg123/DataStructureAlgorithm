c97e84d108743ad796c79014a47381f427b0988e
dzg <1395224335@qq.com>
2019-08-20 00:08:12 +0800

    inValidBST

--- src/com/dzg/Offer/二叉树/二叉搜索树的第K个结点/TreeNode.java
+++ src/com/dzg/Offer/二叉树/二叉搜索树的第K个结点/TreeNode.java
@@ -1,9 +1,9 @@
 package com.dzg.Offer.二叉树.二叉搜索树的第K个结点;
 
 public class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
+    public int val;
+    public TreeNode left = null;
+    public TreeNode right = null;
 
     public TreeNode(int val) {
         this.val = val;
